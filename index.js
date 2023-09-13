9f0e301ec896c00c973fe270425b99c57ddf4d1e
ERYCK KAMILO CARDOSO TIBERIO <erycl26tiberio@gmail.com>
2023-09-12 21:40:03 -0300

    concluido

---
+++ index.js
@@ -0,0 +1,11 @@
+<script>
+    {/* Desafios
+        Fazer um validador de senha 
+    
+    */}
+        function enviar_dados() {
+            //aqui vamos criar a função para enviar ao banco de dados
+        }
+        
+        
+    </script>
